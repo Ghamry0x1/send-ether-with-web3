dfa56119dd7256fde769469c5b04bd7159d21ade
Mohamed El Ghamry <mohamed.elghamry@outlook.com>
2019-02-04 15:32:09 +0200

    make it better
    Signed-off-by: Mohamed El Ghamry <mohamed.elghamry@outlook.com>

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
